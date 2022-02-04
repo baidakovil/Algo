394b891718e82d8b7a3f0dbfd5909c113ff6d185
baidakovil <baidakovil@gmail.com>
2022-02-04 11:41:24 +0300

    Полное решение 312 мс

--- HW5/CutTheTape_v3.py
+++ HW5/CutTheTape_v3.py
@@ -8,22 +8,19 @@ def cut(n_less, x, y):
     while mlt_y <= n_less // y:
         mlt_x = (n_less - mlt_y * y) / x
         if mlt_x.is_integer():
-            return int(mlt_x + mlt_y)
+            return int(mlt_x), int(mlt_y)
         mlt_y += 1
     return False
 
 
 def super_cut(n, abc):
-    try_a_b = cut(n, abc[0], abc[1])
-    if try_a_b:
-        return try_a_b
+    results = []
     a_count = n // abc[0]
-    for i in range(n // abc[0] + 1):
-        n_less = n - a_count * abc[0]
-        try_b_c = cut(n_less, abc[1], abc[2])
-        if try_b_c:
-            return try_b_c + a_count
+    for i in range(a_count + 1):
+        try_b_c = cut(n - a_count * abc[0], abc[1], abc[2])
+        if try_b_c: results.append(a_count + sum(try_b_c))
         a_count -= 1
+    return max(results)
 
 
 print(super_cut(n_inp, sorted([a_inp, b_inp, c_inp])))
